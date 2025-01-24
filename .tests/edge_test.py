810ccf3c2cceebac31d7579867fbe7e7e7a64639
yotam-biu <nagaryo3@biu.ac.il>
2025-01-24 17:35:15 +0200

    Update edge_test.py

--- .tests/edge_test.py
+++ .tests/edge_test.py
@@ -5,12 +5,12 @@ from skimage.morphology import ball
 import numpy as np
 
 def test_edge():
-    image = load_image('lena.jpg')
+    image = load_image('.tests/lena.jpg')
     image = median(image, ball(3))
     edge = edge_detection(image)
     edge_binary = edge > 50
 
-    true = load_image('lena_edges.png')
+    true = load_image('.tests/lena_edges.png')
 
     area = true.shape[0] * true.shape[1]
     score = np.sum(true == edge_binary)/area
